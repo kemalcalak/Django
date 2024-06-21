6f4a6fc4c86b7866117d876554e9080e5f13e4b1
Ali Kemal Çalak <kemalcalak@gmail.com>
2024-06-21 20:28:49 +0300

    Django Forms

--- django-projects2/examples/myapp/urls.py
+++ django-projects2/examples/myapp/urls.py
@@ -1,14 +1,12 @@
 from django.urls import path
 from . import views
 
-# http://127.0.0.1:8000         => index
-# http://127.0.0.1:8000/details => details
-# http://127.0.0.1:8000/list    => list
-
 urlpatterns = [
     path('', views.index),
     path('index', views.index),
+    path('create', views.create),
+    path('edit/<int:id>', views.edit, name="product_edit"),
+    path('delete/<int:id>', views.delete, name="product_delete"),
+    path('list', views.list, name="product_list"),
     path('<slug:slug>', views.details, name="product_details"),
-    path('<int:category_id>', views.getProductsByCategoryId),
-    path('<str:category>', views.getProductsByCategory, name='products_by_category')
 ]
